ab4c3ebb436b397e64a70d55954e750a489ba6f8
Bob Kerns <1154903+BobKerns@users.noreply.github.com>
2025-04-20 12:48:02 -0700

    test_geometry needs update for positional names.

--- test/test_geometries.py
+++ test/test_geometries.py
@@ -5,12 +5,12 @@ Test that supplied geometries are correctly built
 from gltf_builder.geometries import CUBE
 
 def test_geo_cube(test_builder):
-    top = test_builder.create_node(name='TOP')
+    top = test_builder.create_node('TOP')
     top.instantiate(CUBE)
 
 
 def test_geo_cube_detached(test_builder, save):
-    top = test_builder.create_node(name='TOP', detached=True)
+    top = test_builder.create_node('TOP', detached=True)
     top.instantiate(CUBE)
     test_builder.instantiate(top)
     save(test_builder.build())
