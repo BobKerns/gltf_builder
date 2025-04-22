515ac7aabce4ae1a000557bfa619aa9ef1afe555
Bob Kerns <1154903+BobKerns@users.noreply.github.com>
2025-04-22 06:59:51 -0700

    Add color and uv to built-in cube geometry.

--- src/gltf_builder/geometries.py
+++ src/gltf_builder/geometries.py
@@ -7,6 +7,7 @@ from collections.abc import Iterator
 from contextlib import contextmanager
 from typing import Optional
 
+from gltf_builder.attribute_types import color, point, uv, vector3
 from gltf_builder.core_types import JsonObject, NameMode, PrimitiveMode
 from gltf_builder.builder import Builder
 from gltf_builder.element import BNode
@@ -40,10 +41,10 @@ def make(name: str,
     yield node
 
 
-_CUBE = (
+_CUBE = tuple(point(p) for p in (
     (0, 0, 0), (0, 0, 1), (0, 1, 1), (0, 1, 0),
     (1, 0, 0), (1, 0, 1), (1, 1, 1), (1, 1, 0),
-)
+))
 _CUBE_FACE1 = (0, 1, 2, 3)
 _CUBE_FACE2 = (4, 5, 6, 7)
 _CUBE_FACE3 = (0, 4, 5, 1)
@@ -51,12 +52,23 @@ _CUBE_FACE4 = (2, 6, 7, 3)
 _CUBE_FACE5 = (0, 4, 7, 3)
 _CUBE_FACE6 = (1, 5, 6, 2)
 
-_CUBE_NORMAL1 = (1, 0, 0)
-_CUBE_NORMAL2 = (-1, 0, 0)
-_CUBE_NORMAL3 = (0, 1, 0)
-_CUBE_NORMAL4 = (0, -1, 0)
-_CUBE_NORMAL5 = (0, 0, 1)
-_CUBE_NORMAL6 = (0, 0, -1)
+_CUBE_NORMAL1 = vector3(1, 0, 0)
+_CUBE_NORMAL2 = vector3(-1, 0, 0)
+_CUBE_NORMAL3 = vector3(0, 1, 0)
+_CUBE_NORMAL4 = vector3(0, -1, 0)
+_CUBE_NORMAL5 = vector3(0, 0, 1)
+_CUBE_NORMAL6 = vector3(0, 0, -1)
+
+_CUBE_COLORS = (
+    color(0.25, 1.0, 0.0, 1.0),
+    color(1.0, 0.25, 0.0, 1.0),
+    color(1.0, 0.25, 0.25, 1.0),
+    color(0.25, 1.0, 1.0, 1.0),
+    color(0.0, 1.0, 0.0, 1.0),
+    color(1.0, 0.0, 0.0, 1.0),
+    color(0.0, 0.0, 1.0, 1.0),
+    color(1.0, 0.0, 1.0, 1.0),
+)
 
 _CUBE_FACES = (
         (_CUBE_FACE1, _CUBE_NORMAL1),
@@ -72,6 +84,12 @@ with make('CUBE') as cube:
         name = f'FACE{i+1}'
         node = cube.create_node(name)
         mesh = node.create_mesh(name)
-        mesh.add_primitive(PrimitiveMode.LINE_LOOP, *[_CUBE[i] for i in face], NORMAL=4 *(normal,))
+               
+        uvx = uv(0.0, 0.0), uv(0.0, 1.0), uv(1.0, 1.0), uv(1.0, 0.0)
+        mesh.add_primitive(PrimitiveMode.LINE_LOOP, *[_CUBE[i] for i in face],
+                           NORMAL=4 *(normal,),
+                           COLOR_0=[_CUBE_COLORS[i] for i in face],
+                           TEXCOORD_0= uvx
+                           )
     CUBE = cube
 
