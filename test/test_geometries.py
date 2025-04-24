083886b11cc6beae2eaccb0d1ff6ea8e611c0302
Bob Kerns <1154903+BobKerns@users.noreply.github.com>
2025-04-24 09:11:42 -0700

    Geometry tests call node() directly.

--- test/test_geometries.py
+++ test/test_geometries.py
@@ -3,16 +3,17 @@ Test that supplied geometries are correctly built
 '''
 
 from gltf_builder.geometries import get_geometry
+from gltf_builder.nodes import node
 
 def test_geo_cube(test_builder):
     CUBE = get_geometry('CUBE')
-    top = test_builder.create_node('TOP')
+    top = node('TOP')
     top.instantiate(CUBE)
 
 
 def test_geo_cube_detached(test_builder, save):
     CUBE = get_geometry('CUBE')
-    top = test_builder.create_node('TOP', detached=True)
+    top = node('TOP')
     top.instantiate(CUBE)
     test_builder.instantiate(top)
     save(test_builder.build())
