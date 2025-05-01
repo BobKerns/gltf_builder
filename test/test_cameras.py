a18a9916940992b5bc301c2d7a52407204add875
Bob Kerns <1154903+BobKerns@users.noreply.github.com>
2025-05-01 13:08:23 -0700

    Add camera tests that led to fixes.

--- test/test_cameras.py
+++ test/test_cameras.py
@@ -2,6 +2,8 @@
 Test cases for the cameras module.
 '''
 
+import math
+from typing import cast
 import pytest
 
 import pygltflib as gltf
@@ -11,6 +13,9 @@ from gltf_builder import (
 )
 
 from conftest import TEST_EXTRAS, TEST_EXTENSIONS, BuilderContext, GeometryData
+from gltf_builder.core_types import IndexSize
+from gltf_builder.elements import BOrthographicCamera, BPerspectiveCamera
+from gltf_builder.utils import count_iter, first, index_of
 
 
 def test_camera_perspective(test_builder: BuilderContext):
@@ -18,7 +23,7 @@ def test_camera_perspective(test_builder: BuilderContext):
     Test that a perspective camera is correctly built.
     '''
     cam = camera(CameraType.PERSPECTIVE, 'Camera',
-                    yfov=45.0,
+                    yfov=math.pi / 6,
                     znear=0.01,
                     zfar=1000.0,
                     aspectRatio=1.0,
@@ -28,7 +33,7 @@ def test_camera_perspective(test_builder: BuilderContext):
     assert cam.name == 'Camera'
     assert cam._scope_name == 'camera'
     assert cam.aspectRatio == 1.0
-    assert cam.yfov == 45.0
+    assert cam.yfov == math.pi / 6
     assert cam.znear == 0.01
     assert cam.zfar == 1000.0
     assert cam.extras == TEST_EXTRAS
@@ -36,7 +41,7 @@ def test_camera_perspective(test_builder: BuilderContext):
     assert cam.perspective is not None
     assert cam.orthographic is None
     assert cam.perspective.aspectRatio == 1.0
-    assert cam.perspective.yfov == 45.0
+    assert cam.perspective.yfov == math.pi / 6
     assert cam.perspective.znear == 0.01
     assert cam.perspective.zfar == 1000.0
     assert cam.perspective.extras == {}
@@ -78,8 +83,8 @@ def test_camera_orthographic(test_builder: BuilderContext):
 
 
 @pytest.mark.parametrize('test_camera', [
-    camera(CameraType.PERSPECTIVE),
-    camera(CameraType.ORTHOGRAPHIC),
+    camera(CameraType.PERSPECTIVE, 'perspective'),
+    camera(CameraType.ORTHOGRAPHIC, 'orthographic'),
 ])
 def test_camera_node(cube: GeometryData,
                      index_sizes: tuple[IndexSize, int, int],
@@ -101,3 +106,15 @@ def test_camera_node(cube: GeometryData,
             for n in g.nodes
             if n.camera is not None
         ) == 1
+
+        cameras: list[gltf.Camera] = g.cameras
+        cam_idx = index_of(
+            cast(list[gltf.Camera], cameras),
+            value_eq=test_camera.name,
+            attribute='name',
+        )
+        cam = cameras[cam_idx]
+        assert cam.name == test_camera.name
+        node = first(n for n in g.nodes if n.name == 'CameraNode')
+        assert node.camera == cam_idx
+        assert node.translation == [0, 0, -10]
