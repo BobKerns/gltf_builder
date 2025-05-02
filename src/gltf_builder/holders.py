8de1424b33b0817c4f219150382430db41cf71b4
Bob Kerns <1154903+BobKerns@users.noreply.github.com>
2025-05-02 01:44:36 -0700

    Split out immutable _RO_Holder

--- src/gltf_builder/holders.py
+++ src/gltf_builder/holders.py
@@ -12,42 +12,34 @@ if TYPE_CHECKING:
 else:
     T = TypeVar('T')
 
-class _Holder(Iterable[T]):
+class _RO_Holder(Iterable[T]):
     '''
-    A container for `Element` instances, indexable by index or name.
-    This also guarantees an item is added only once.
+    A read-only container for `Element` instances, indexable by index or name.
     '''
     __type: type[T]
-    __by_index: list[T]
-    __by_name: dict[str, T]
-    __by_value: set[T]
-    def __init__(self, type_: type[T], *items: T):
-        self.__type = type_
-        self.__by_index = []
-        self.__by_name = {}
-        self.__by_value = set()
-        self.add_from(items)
+    @property
+    def type(self) -> type[T]:
+        '''
+        The type of the items in the holder.
+        '''
+        return self.__type
+    _by_index: list[T]
+    _by_name: dict[str, T]
 
-    def add(self, item: T):
-        '''
-        Add an item to the holder, if not already present.
-        '''
-        if item not in self.__by_value:
-            self.__by_value.add(item)
-            self.__by_index.append(item)
-            if item.name:
-                self.__by_name[item.name] = item
+    def __init__(self, type_: 'type[T]', *items: T):
+        self.__type = type_
+        self._by_index = []
+        self._by_name = {}
+        self.add_from(items)
 
     def add_from(self, items: Iterable[T]):
         '''
-        Add items to the holder, if not already present.
+        Add items to the holder.
         '''
         for item in items:
-            if item not in self.__by_value:
-                self.__by_value.add(item)
-                self.__by_index.append(item)
-                if item.name:
-                    self.__by_name[item.name] = item
+            self._by_index.append(item)
+            if item.name:
+                self._by_name[item.name] = item
 
     @overload
     def get(self, key: str|int, default: T) -> T: ...
@@ -63,47 +55,87 @@ class _Holder(Iterable[T]):
             return self[key]
         except KeyError:
             return default
-
     def __iter__(self):
         '''
         We can iterate over all items in the `Holder`.
         '''
-        return iter(self.__by_index)
-
+        return iter(self._by_index)
     def __getitem__(self, key: str|int) -> T:
         '''
         We can get items by index (position) or name, if named.
         '''
         if isinstance(key, str):
-            return self.__by_name[key]
-        return self.__by_index[key]
+            return self._by_name[key]
+        return self._by_index[key]
 
     def __len__(self):
         '''
-        The number of items held.
+        The number of items in the `Holder`.
         '''
-        return len(self.__by_index)
-
-    def __contains__(self, item: T|str|int):
-        '''
-        Return `True` if the item, it's name, or its index is present.
-        '''
-        match item:
-            case str():
-                return item in self.__by_name
-            case int():
-                return item >= 0 and item < len(self)
-            case _:
-                return item in self.__by_index
+        return len(self._by_index)
 
     def __bool__(self):
         '''
-        Return `True` if the holder is not empty.
+        The `Holder` is truthy if it contains items.
         '''
-        return bool(self.__by_index)
+        return bool(self._by_index)
+
+
+class _Holder(_RO_Holder[T]):
+    '''
+    A container for `Element` instances, indexable by index or name.
+    This also guarantees an item is added only once.
+    '''
+    def __init__(self, type_: type[T], *items: T):
+        super().__init__(type_)
+        self.add_from(items)
+
+    def add(self, item: T):
+        '''
+        Add an item to the holder, if not already present.
+        '''
+        if item not in self._by_index:
+            self._by_index.append(item)
+            if item.name:
+                self._by_name[item.name] = item
+
+    def add_from(self, items: Iterable[T]):
+        '''
+        Add items to the holder, if not already present.
+        '''
+        for item in items:
+            if item not in self._by_index:
+                self._by_index.append(item)
+                if item.name:
+                    self._by_name[item.name] = item
+
+    def __setitem__(self, key: str|int, value: T):
+        '''
+        We can set items by index (position) or name, if named.
+        '''
+        if isinstance(key, str):
+            self._by_name[key] = value
+            if value not in self._by_index:
+                self._by_index.append(value)
+        else:
+            self._by_index[key] = value
+            if value.name:
+                self._by_name[value.name] = value
+
+    def __delitem__(self, key: str|int):
+        '''
+        We can delete items by index (position) or name, if named.
+        '''
+        if isinstance(key, str):
+            item = self._by_name.pop(key)
+            self._by_index.remove(item)
+        else:
+            item = self._by_index.pop(key)
+            if item.name:
+                self._by_name.pop(item.name, None)
 
     def __repr__(self):
         '''
         A string representation of the `Holder`.
         '''
-        return f'<{self.__class__.__name__}({self.__type.__name__}, {len(self)})>'
+        return f'<{self.__class__.__name__}[{self.type.__name__}]({len(self)})>'
